3352ebfc838c9c71ef112d1c67bb4695f550b5f1
Constance Ye <constanceye1316@gmail.com>
2017-11-02 20:41:38 -0400

    update links

--- code/p5/spin-ball/sketch.js
+++ code/p5/spin-ball/sketch.js
@@ -1,41 +1,20 @@
-var counter2 = 0;
+var img;
 
+function preload(){
+    img = loadImage("http://food.fnr.sndimg.com/content/dam/images/food/editorial/talent/guy-fieri/FN-TalentAvatar-Guy-Fieri-800x800.jpg.rend.hgtvcom.616.616.suffix/1457720995801.jpeg");
+}
 
 function setup() {
-
-    createCanvas(windowWidth, windowHeight);
-    background(0);
-    ellipseMode(RADIUS);
-
+  createCanvas(windowWidth, windowHeight);
+  background(200);
 }
-
 
 function draw() {
-        background(0);
-    textSize(12);
-    colorMode(RGB, 255, 255, 255);
-    fill(175, 175, 175);
-    text("press down and hold to stop...", windowWidth/2 - 75, windowHeight - 70);
-    drawGradient(windowWidth/2, windowHeight/2, random(130, 150), counter2);
-    counter2+= 2;
-    fill(255);
-
-}
-function mousePressed(){
-noLoop();
-}
-function mouseReleased(){
-loop();
 }
 
-function drawGradient(x, y, ran, comp) {
-    colorMode(HSB, 360, 100, 100);
-    noStroke();
-    ellipseMode(RADIUS);
-    var radius = 160;
-    for (var r = radius; r > 0; --r) {
-        fill(ran, 200 - 2 * r, 200 - 2 * r);
-        ellipse(x, y, r, r);
-        ran = (ran + comp) % 360;
-    }
+//stops the draw function from looping when you press down
+//a function in p5.js! no need for eventlistener! nice!
+function mousePressed() {
+  
 }
+
